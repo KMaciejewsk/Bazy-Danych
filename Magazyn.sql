27411c44de1fbb38701c55a3619164d6f31fc995
Banano <115485162+KMaciejewsk@users.noreply.github.com>
2023-06-11 20:54:16 +0200

    Update Magazyn.sql

--- Magazyn.sql
+++ Magazyn.sql
@@ -45,14 +45,6 @@ CREATE TABLE Magazyn (
   FOREIGN KEY (ID_adresu) REFERENCES Adres(ID_adresu)
 );
 
-CREATE TABLE Magazyn_Kategoria (
-  ID_magazynu INT,
-  ID_kategorii INT,
-  PRIMARY KEY (ID_magazynu, ID_kategorii),
-  FOREIGN KEY (ID_magazynu) REFERENCES Magazyn(ID_magazynu),
-  FOREIGN KEY (ID_kategorii) REFERENCES Kategoria(ID_kategorii)
-);
-
 CREATE TABLE Zamówienie_Produkt (
   ID_zamówienia INT,
   ID_produktu INT,
@@ -61,24 +53,10 @@ CREATE TABLE Zamówienie_Produkt (
   FOREIGN KEY (ID_produktu) REFERENCES Produkt(ID_produktu)
 );
 
--- Dodawanie kategorii
-INSERT INTO Kategoria (ID_kategorii, Nazwa_kategorii)
-VALUES (1, 'Kategoria 1'), (2, 'Kategoria 2');
-
--- Dodawanie produktów dla Kategorii 1
-INSERT INTO Produkt (ID_produktu, Nazwa, Cena, Opis, ID_kategorii)
-VALUES
-  (1, 'Produkt 1', 10.99, 'Opis produktu 1', 1),
-  (2, 'Produkt 2', 15.99, 'Opis produktu 2', 1),
-  (3, 'Produkt 3', 20.99, 'Opis produktu 3', 1),
-  (4, 'Produkt 4', 12.99, 'Opis produktu 4', 1),
-  (5, 'Produkt 5', 9.99, 'Opis produktu 5', 1);
-
--- Dodawanie produktów dla Kategorii 2
-INSERT INTO Produkt (ID_produktu, Nazwa, Cena, Opis, ID_kategorii)
-VALUES
-  (6, 'Produkt 6', 8.99, 'Opis produktu 6', 2),
-  (7, 'Produkt 7', 14.99, 'Opis produktu 7', 2),
-  (8, 'Produkt 8', 18.99, 'Opis produktu 8', 2),
-  (9, 'Produkt 9', 11.99, 'Opis produktu 9', 2),
-  (10, 'Produkt 10', 7.99, 'Opis produktu 10', 2);
+CREATE TABLE Magazyn_Kategoria (
+  ID_magazynu INT,
+  ID_kategorii INT,
+  PRIMARY KEY (ID_magazynu, ID_kategorii),
+  FOREIGN KEY (ID_magazynu) REFERENCES Magazyn(ID_magazynu),
+  FOREIGN KEY (ID_kategorii) REFERENCES Kategoria(ID_kategorii)
+);
